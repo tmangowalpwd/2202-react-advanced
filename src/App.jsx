9f914cd7a046972fc4b55df811934fc630f61a62
theodevoid <87703322+theodevoid@users.noreply.github.com>
2022-09-06 14:35:38 +0700

    keep login

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,56 @@
-import { Box, Text } from "@chakra-ui/react"
-import { useSelector } from "react-redux"
+import { Box, Button, HStack, Text } from "@chakra-ui/react"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
 import { Routes, Route } from "react-router-dom"
+import { axiosInstance } from "./api"
 import HomePage from "./pages/Home"
 import LoginPage from "./pages/Login"
 import RegisterPage from "./pages/Register"
+import { login, logout } from "./redux/features/authSlice"
 
 const App = () => {
   const authSelector = useSelector((state) => state.auth)
+  const dispatch = useDispatch()
+
+  const keepUserLoggedIn = async () => {
+    try {
+      const auth_id = localStorage.getItem("auth_data")
+
+      if (!auth_id) {
+        return
+      }
+
+      const response = await axiosInstance.get(`/users/${auth_id}`)
+
+      dispatch(login(response.data))
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const logoutBtnHandler = () => {
+    localStorage.removeItem("auth_data")
+    dispatch(logout())
+  }
+
+  useEffect(() => {
+    keepUserLoggedIn()
+  }, [])
 
   return (
     <Box>
-      <Text fontSize="4xl" fontWeight="bold">
-        Hello World {authSelector.id}
-      </Text>
+      <Box backgroundColor="teal" color="white" px="8" py="4">
+        <HStack justifyContent="space-between">
+          <Text fontSize="4xl" fontWeight="bold">
+            Hello {authSelector.username}
+          </Text>
+          <Box>
+            <Button onClick={logoutBtnHandler} colorScheme="red">
+              Logout
+            </Button>
+          </Box>
+        </HStack>
+      </Box>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
--- src/redux/features/authSlice.js
+++ src/redux/features/authSlice.js
@@ -10,9 +10,24 @@ const initialState = {
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {},
+  reducers: {
+    login: (state, action) => {
+      state.id = action.payload.id
+      state.username = action.payload.username
+      state.email = action.payload.email
+      state.role = action.payload.role
+    },
+    logout: (state) => {
+      state.id = 0
+      state.username = ""
+      state.role = ""
+      state.email = ""
+
+      // return initialState
+    }
+  },
 })
 
-export const { } = authSlice.actions
+export const { login, logout } = authSlice.actions
 
 export default authSlice.reducer
